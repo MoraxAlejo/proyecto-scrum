8edefeab0654ea116b2f7d1a66b3c83a0afb0df9
jarvin <mangomc03@gmail.com>
2023-09-25 18:32:19 -0400

    first commit

---
+++ src/views/login.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/dist/output.css" rel="stylesheet">
+  <title>Funval Internacional: Entrar al sitio</title>
+</head>
+
+<body>
+  <main class="h-screen flex justify-center items-center bg-[url('/img/fondo.png')] bg-cover">
+    <div class="bg-white">
+      <h1 class=" text-3xl font-bold">
+        Hello world!
+      </h1>
+    </div>
+  </main>
+</body>
+
+</html>
